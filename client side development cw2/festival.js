2ca7a171a6d682362c5def67baa463b3873405bf
sudikshyachaulagain-sudi <sudikshyachaulagain2@gmail.com>
2026-04-21 11:01:04 +0100

    Delete client side development cw2 directory

--- client side development cw2/festival.js
+++
@@ -1,9 +0,0 @@
-function toggleText(id) {
-    var text = document.getElementById(id);
-
-    if (text.style.display === "none" || text.style.display === "") {
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
-}
--- client side development cw2/food.js
+++
@@ -1,4 +0,0 @@
-function toggleFood(id) {
-    var text = document.getElementById(id);
-    text.style.display = (text.style.display === "none") ? "block" : "none";
-}
